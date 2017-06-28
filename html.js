a9cedc3500114183e23db2d151e47a63fdaa49c4
Nick Lewis <nickeblewis@gmail.com>
2017-06-28 14:06:07 +0100

    google analytics

--- html.js
+++ html.js
@@ -18,7 +18,8 @@ export default class HTML extends React.Component {
     const { body } = this.props
     const head = Helmet.rewind()
 
-    let css
+    let css, ga;
+
     if (process.env.NODE_ENV === 'production') {
       css = (
         <style
@@ -27,6 +28,12 @@ export default class HTML extends React.Component {
           }}
         />
       )
+    }
+
+    if ( process.env.NODE_ENV !== 'production' || config.buildSPA ) {
+      js =  <script src={ prefixLink(`/bundle.js?t=${BUILD_TIME}`) }/>;
+    } else {
+      ga = <script dangerouslySetInnerHTML={ {__html: require('!raw!./static/ga-snippet.js').replace('UA-XXXXX-Y', config.googleAnalyticsId)} }></script>;
     }
 
     return (
@@ -48,7 +55,17 @@ export default class HTML extends React.Component {
             id="react-mount"
             dangerouslySetInnerHTML={{ __html: body }}
           />
+          { ga }
           <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
+          <script dangerouslySetInnerHTML={{__html: `
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-83176608-1', 'auto');
+            ga('send', 'pageview');`
+          }}
+        />
         </body>
       </html>
     )
