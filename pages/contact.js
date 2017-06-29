c49ab5ce5061fb4052aed53ed1b3515ff6d64f47
Nick Lewis <nick@headforcode.com>
2017-06-29 14:08:35 +0100

    Changes

--- pages/contact.js
+++ pages/contact.js
@@ -6,19 +6,11 @@ import Helmet from 'react-helmet'
 import { config } from 'config'
 import sortBy from 'lodash/sortBy'
 import get from 'lodash/get'
-import include from 'underscore.string/include'
 import { JumboTron, IntroBlock } from 'components'
 // import IntroBlock from 'components/IntroBlock'
 
 class Index extends React.Component {
   render() {
-    // Sort pages.
-      console.log(this.props)
-    const sortedPages = sortBy(this.props.route.pages, 'data.date')
-    // Posts are those with md extension that are not 404 pages OR have a date (meaning they're a react component post).
-    const visiblePages = sortedPages.filter(page => (
-      get(page, 'file.ext') === 'md' && !include(page.path, '/404') && get(page, 'data.type' === 'post' ) || get(page, 'data.date')
-    ))
     return (
       <div className="ph3 bg-off-white">
 	      <div className="center mw6 pv3">
@@ -38,7 +30,7 @@ class Index extends React.Component {
 
 			<div>
 				<h4 className="f4 b lh-title mb2 primary">Working hours</h4>
-				<p>Monday – Friday: 8AM – 4PM We’re closed weekends</p>
+				<p>Monday – Friday: 9AM – 5PM We’re closed weekends</p>
 			</div>
 
 		</div>
