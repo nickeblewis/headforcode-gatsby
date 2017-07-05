cd630f1583554667e2b0cc94f84121f7f46318f9
Nick Lewis <nickeblewis@gmail.com>
2017-07-05 10:32:14 +0100

    removed redundant code and added some comments regarding ideas that I have

--- wrappers/md.js
+++ wrappers/md.js
@@ -1,3 +1,11 @@
+/**
+ * Ideas for FrontMatter
+ * - Tags and categories
+ * - Type - blog/post, article, doc, project, example, tutorial as these later may have unique templates of their own?
+ * - repo - the github repository split by branch for each step, useful for tutorials
+ * - date - currently anything with a date gets listed on the homepage
+ */
+
 import React from 'react'
 import PropTypes from 'prop-types'
 import 'css/markdown-styles.css'
@@ -8,9 +16,6 @@ import ReactDisqus from 'react-disqus'
 import '../css/styles.css'
 
 class Markdown extends React.Component {
-  handleNewCommen(comment) {
-		console.log(comment.text);
-	}
   render() {
     const { route } = this.props
     const post = this.props.route.page.data
@@ -18,6 +23,7 @@ class Markdown extends React.Component {
       <div className="markdown mw6 center ph3 pv4">
         <Helmet title={`${config.siteTitle} | ${post.title}`} />
         <h2 className="f3 b lh-title mb1 primary">{post.title}</h2>
+        { /* TODO: I want to show the author title here and a link to the GitHub repo the article may relate to */ }
         <div dangerouslySetInnerHTML={{ __html: post.body }} />
         <ReactDisqus shortname="headforcode-1" />
       </div>
