fc598a9f10b2b0983f188f5872cd958a8ffaf159
Nick Lewis <nick@headforcode.com>
2017-07-11 22:35:11 +0100

    stuff

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,13 +1,16 @@
 import React from "react"
+import '../css/markdown-styles.css'
+import ReactDisqus from 'react-disqus'
 
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
 
     return (
-      <div>
-        <h1>{post.frontmatter.title}</h1>
+      <div className="markdown mw6 center ph3 pv4">
+        <h2 className="f3 b lh-title mb1 primary">{post.frontmatter.title}</h2>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <ReactDisqus shortname="headforcode-1" />
       </div>
     )
   }
