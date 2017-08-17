82fde95fd63d7ff5a2d5b3f45e91cc3aad82f6b2
Nick Lewis <nickeblewis@gmail.com>
2017-08-17 09:12:47 +0100

    Had to take out Disqus as its not compatible at the moment

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,6 +1,6 @@
 import React from "react"
 import '../css/markdown-styles.css'
-import ReactDisqus from 'react-disqus'
+// import ReactDisqus from 'react-disqus'
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -10,7 +10,7 @@ class BlogPostTemplate extends React.Component {
       <div className="markdown mw6 center ph3 pv4">
         <h2 className="f3 b lh-title mb1 primary">{post.frontmatter.title}</h2>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <ReactDisqus shortname="headforcode-1" />
+        {/* <ReactDisqus shortname="headforcode-1" /> */}
       </div>
     )
   }
